277f52d29e1e6f6af8cb61b295ec1d0709e34ad0
lobos <lobos841@gmail.com>
2012-12-04 15:19:51 +0800

    继续调整

--- lessc.py
+++ lessc.py
@@ -3,5 +3,6 @@
 import os
 
 if __name__ == '__main__':
-    os.system('lessc %s %s -x' % ('css/docs.less', 'css/docs.css'))
+    os.system('lessc %s %s -x' % ('less/mooui.less', 'css/mooui.css'))
+    os.system('lessc %s %s -x' % ('docs/assets/css/docs.less', 'docs/assets/css/docs.css'))
 
